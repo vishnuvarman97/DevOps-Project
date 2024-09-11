04b5771558ec184c665be912d9df3c49eddde33d
vishnuvarman97 <148313919+vishnuvarman97@users.noreply.github.com>
2024-09-12 00:59:08 +0530

    main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,24 +1,15 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+resource "aws_instance" "demo_server" {
+  ami           = "ami-0781c11a831c5f434"
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
-
-  tags = {
-    Environment = "dev"
-    Name = "${var.name}-server"
+  key_name       = "ubuntu"
+  
+  # Optionally specify an EBS volume
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp2"
   }
 }
